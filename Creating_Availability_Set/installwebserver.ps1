a498fef547fda33ab55f4861c7f9cb3e4d6d1c63
karthick suresh <105416723+KarthickSuresh09@users.noreply.github.com>
2025-08-31 13:19:30 +0530

    Delete Creating_Availability_Set directory

--- Creating_Availability_Set/installwebserver.ps1
+++
@@ -1,4 +0,0 @@
-Install-WindowsFeature -Name Web-Server -IncludeManagementTools
-Set-Location -Path C:\inetpub\wwwroot
-Add-Content -Path iisstart.htm "<H1><center>Welcome, Azure Rocks!!!</center></H1>"
-Invoke-Command -ScriptBlock {iisreset}
